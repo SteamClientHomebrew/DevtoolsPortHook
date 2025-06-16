982509d9340593fd20d60abab79246d0e8e1ee60
shadow <81448108+shdwmtr@users.noreply.github.com>
2025-06-15 22:19:22 -0300

    feat: Use 8080 when -dev, random when launching normally.

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,44 @@
+/**
+ * ==================================================
+ *   _____ _ _ _             _                     
+ *  |     |_| | |___ ___ ___|_|_ _ _____           
+ *  | | | | | | | -_|   |   | | | |     |          
+ *  |_|_|_|_|_|_|___|_|_|_|_|_|___|_|_|_|          
+ * 
+ * ==================================================
+ * 
+ * Copyright (c) 2025 Project Millennium
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#define WIN32_LEAN_AND_MEAN 
+#include <winsock2.h>
+#define _WINSOCKAPI_
 #include <windows.h>
 #include <winternl.h>
 #include <iostream>
+#include <asio.hpp>
+#include <asio/ip/tcp.hpp>
 #include "MinHook.h"
 #define LDR_DLL_NOTIFICATION_REASON_LOADED   1
+#define DEFAULT_DEVTOOLS_PORT "8080"
 
 typedef struct _LDR_DLL_NOTIFICATION_DATA {
     ULONG Flags;
@@ -21,22 +57,46 @@ LdrRegisterDllNotification_t LdrRegisterDllNotification = nullptr;
 LdrUnregisterDllNotification_t LdrUnregisterDllNotification = nullptr;
 PVOID g_NotificationCookie = nullptr;
 
-typedef const char* (__stdcall* Plat_CommandLineParamValue_t)(const char* param);
+typedef LPCSTR (__attribute__((__cdecl__))* Plat_CommandLineParamValue_t)(LPCSTR param);
+// No idea why then made this return a char? boolean and char are the same size on x86/x64, so this is useless.
+typedef CHAR (__attribute__((__cdecl__))* Plat_CommandLineParamExists_t)(LPCSTR param); 
 
 Plat_CommandLineParamValue_t fpOriginalPlat_CommandLineParamValue = nullptr;
+Plat_CommandLineParamExists_t fpPlat_CommandLineParamExists = nullptr;
 
-const char* __stdcall Hooked_Plat_CommandLineParamValue(const char* param) {
-    if (strcmp(param, "-devtools-port") == 0) 
-        return "9222";
-    
+asio::ip::port_type GetRandomOpenPort() {
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor(io_context);
+    asio::ip::tcp::endpoint endpoint(asio::ip::tcp::v4(), 0);
+
+    acceptor.open(endpoint.protocol());
+    acceptor.bind(endpoint);
+    acceptor.listen();
+    return acceptor.local_endpoint().port();
+}
+
+HMODULE steamTier0Module;
+
+LPCSTR Hooked_Plat_CommandLineParamValue(LPCSTR param) {
+    if (strcmp(param, "-devtools-port") == 0) {
+        static const std::string RANDOM_OPEN_PORT = std::to_string(GetRandomOpenPort());
+
+        FARPROC pFunc = GetProcAddress(steamTier0Module, "Plat_CommandLineParamExists");
+        if (pFunc == nullptr) return RANDOM_OPEN_PORT.c_str();
+
+        fpPlat_CommandLineParamExists = reinterpret_cast<Plat_CommandLineParamExists_t>(pFunc);
+        if (fpPlat_CommandLineParamExists == nullptr || !fpPlat_CommandLineParamExists("-dev")) return RANDOM_OPEN_PORT.c_str();
+        
+        return DEFAULT_DEVTOOLS_PORT; // Default port if -dev is present
+    }
     return fpOriginalPlat_CommandLineParamValue(param);
 }
 
 VOID HandleTier0Dll(PVOID moduleBaseAddress) {
     if (MH_Initialize() != MH_OK) return;
     
-    HMODULE hModule = (HMODULE)moduleBaseAddress;
-    FARPROC pFunc = GetProcAddress(hModule, "Plat_CommandLineParamValue");
+    steamTier0Module = (HMODULE)moduleBaseAddress;
+    FARPROC pFunc = GetProcAddress(steamTier0Module, "Plat_CommandLineParamValue");
 
     if (pFunc == nullptr) return;
     if (MH_CreateHook(reinterpret_cast<LPVOID>(pFunc), reinterpret_cast<LPVOID>(&Hooked_Plat_CommandLineParamValue), reinterpret_cast<LPVOID*>(&fpOriginalPlat_CommandLineParamValue)) != MH_OK) return;
@@ -51,7 +111,7 @@ VOID CALLBACK DllNotificationCallback(ULONG NotificationReason, PLDR_DLL_NOTIFIC
         HandleTier0Dll(NotificationData->DllBase);
 }
 
-bool InitializeDllNotification() {
+BOOL InitializeDllNotification() {
     HMODULE ntdllModule = GetModuleHandleA("ntdll.dll");
     if (!ntdllModule) return false;
     
@@ -62,14 +122,23 @@ bool InitializeDllNotification() {
     return NT_SUCCESS(LdrRegisterDllNotification(0, DllNotificationCallback, nullptr, &g_NotificationCookie));
 }
 
-void CleanupDllNotification() {
+VOID CleanupDllNotification() {
     if (g_NotificationCookie && LdrUnregisterDllNotification) {
         LdrUnregisterDllNotification(g_NotificationCookie);
         g_NotificationCookie = nullptr;
     }
 }
 
+BOOL IsSteamClient() {
+    CHAR p[MAX_PATH], *n;
+    GetModuleFileNameA(0, p, MAX_PATH);
+    n = strrchr(p, '\\');
+    return !_stricmp(n ? n + 1 : p, "steam.exe");
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    if (!IsSteamClient()) return TRUE; // Not a Steam client, no need to proceed
+    
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH: InitializeDllNotification(); break;
         case DLL_PROCESS_DETACH: CleanupDllNotification();    break;
